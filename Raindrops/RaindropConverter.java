de07abc3446d7a9a1bd994062bb984dde5297193
gowkoki <91251816+gowkoki@users.noreply.github.com>
2025-06-16 12:41:30 +0100

    Create RaindropConverter.java

---
+++ Raindrops/RaindropConverter.java
@@ -0,0 +1,18 @@
+class RaindropConverter {
+
+    String convert(int number) {
+        String result ="";
+        if(number%3 ==0){
+            result+= "Pling";
+        } 
+        if(number%5 ==0){
+            result+= "Plang";
+        }
+        if(number%7 ==0){
+            result+= "Plong";
+        }
+        
+        return result.length() > 0 ? result : Integer.toString(number);
+    }
+
+}
